4ddbb5295b21ea58ff57863ba46f439957bf71ba
kamrunnahar_ruma <126423515+kamrunnahar-ruma@users.noreply.github.com>
2025-08-11 01:38:29 +0600

    Add files via upload

---
+++ Lab4.asm
@@ -0,0 +1,46 @@
+.model small
+.stack 100h
+.data
+    msg1 db 'Enter bit pattern: $'
+    msg2 db 13,10,'Reversed bit pattern: $'
+.code
+main proc
+    mov ax, @data
+    mov ds, ax
+
+    lea dx, msg1
+    mov ah, 9
+    int 21h
+
+read_loop:
+    mov ah, 1
+    int 21h
+    cmp al, 13
+    je print_reverse
+    cmp al, '0'
+    jb read_loop
+    cmp al, '1'
+    ja read_loop
+    push ax
+    jmp read_loop
+
+print_reverse:
+    lea dx, msg2
+    mov ah, 9
+    int 21h
+
+print_loop:
+    cmp sp, 100h
+    je done
+    pop ax
+    mov dl, al
+    mov ah, 2
+    int 21h
+    jmp print_loop
+
+done:
+    mov ah, 4ch
+    int 21h
+
+main endp
+end main
